b04b289b25dff8ed232fe9dd3d61b636ff59385a
Andrew Watson <aww@awwsmm.com>
2021-01-24 11:07:32 +0000

    add blacklist functionality and output to monthly.py

--- monthly.py
+++ monthly.py
@@ -24,8 +24,32 @@ reddit = praw.Reddit(
              password      = config['password'])
 
 dt = datetime.datetime.today()
+counter = 1
+
+with open('blacklist.json') as f:
+    blacklist = json.load(f)
+
+def is_blacklisted(artist, song):
+
+    def fuzzy_intersection(given, blacklisted):
+        given_lower = given.lower()
+        blacklisted_lower = blacklisted.lower()
+        return (given_lower in blacklisted_lower or blacklisted_lower in given_lower)
+
+    for track in blacklist:
+        if (fuzzy_intersection(artist, track['artist']) and fuzzy_intersection(song, track['song'])):
+          return True;
+    return False;
 
 with open(f'monthly/{dt.year}-{dt.month:02}-{dt.day:02}.txt', 'w') as outfile:
     for submission in reddit.subreddit("nearprog").top("month"):
-        print(f'{submission.score:3} | {submission.title}', file=outfile)
+        if (submission.link_flair_text != 'Discussion'):
+            title_parts = submission.title.split(' - ')
+            artist = title_parts[0]
+            song = title_parts[1]
 
+            if (is_blacklisted(artist, song)):
+                print(f' ---)  --^ | [[BLACKLISTED]] {submission.title}', file=outfile)
+            else:
+                print(f' {counter:3})  {submission.score:2}^ | {submission.title}', file=outfile)
+                counter += 1
