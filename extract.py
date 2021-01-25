2e26546f3bcee19344ed9272f0e62c2bae1c14df
Andrew Watson <aww@awwsmm.com>
2021-01-25 21:38:28 +0000

    add 'flatten' function to flatten arrays of arrays, 'multiple_artists()' function to try to extract multiple artists from a single artist string, more test cases to 'parenthetical_subgenre()'

--- extract.py
+++ extract.py
@@ -11,6 +11,55 @@ def tests(arr):
         for elem in arr:
             print(elem,'=',eval(elem))
 
+# flatten an array of arrays, ex. [[1, 2], [3], [4, 5, 6]] => [1, 2, 3, 4, 5, 6]
+def flatten(arrarr):
+    return [item for sublist in arrarr for item in sublist]
+
+tests([
+    'flatten([[1, 2], [3], [4, 5, 6]])',
+    'flatten([["Eminem"], ["Juice WRLD"]])'
+    ])
+
+# try to extract multiple artist names
+def multiple_artists(artist):
+
+    # split on " ft." / " feat."
+    artists = re.split('\s+[Ff]eat.\s*|\s+[Ff]t.\s*', artist)
+
+    # don't split artist names like "Black Country, New Road" and "Simon and Garfunkel"
+    with open('compound_artists.json') as f:
+        compound_artists = json.load(f)
+
+    # split if not a compound artist
+    def is_compound(artist):
+        return fuzzy.contains(compound_artists, artist)
+
+    # split on "and" except when followed by "the", like "Jim and the Other Guys"
+    # split on ","
+    def split_if_not_compound(artist):
+        if (is_compound(artist)):
+            return [artist]
+        return re.split(',?\s+[Aa][Nn][Dd]\s+(?!the)|\s*,\s*', artist)
+
+    return flatten(utils.foreach(artists, lambda x: split_if_not_compound(x)))
+
+tests([
+    'multiple_artists("Eminem ft. Juice WRLD")',
+    'multiple_artists("Korn feat. Ice Cube")',
+    'multiple_artists("Emanative feat. Ahu")',
+    'multiple_artists("Flight Facilities feat. Broods, Reggie Watts, and Saro")',
+    'multiple_artists("David Bowie feat. Maynard James Keenan and John Frusciante")',
+    'multiple_artists("Intervals feat. Plini")',
+    'multiple_artists("Flying Lotus feat. Kendrick Lamar")',
+    'multiple_artists("Efterklang")',
+    'multiple_artists("Dave Grohl, Trent Reznor, Josh Homme")',
+    'multiple_artists("Black Country, New Road")',
+    'multiple_artists("King Gizzard and the Lizard Wizard")',
+    'multiple_artists("The Third and the Mortal")',
+    'multiple_artists("Professor Caffeine and the Insecurities")',
+    'multiple_artists("Simon and Garfunkel")'
+    ])
+
 # try to extract (subgenre) in () instead of []
 def parenthetical_subgenre(song):
 
@@ -52,5 +101,13 @@ tests([
     'parenthetical_subgenre("Hello (It\'s Me)")',
     'parenthetical_subgenre("Goodbye (post-rock)")',
     'parenthetical_subgenre("Whaddup (post-jazz metal fusion / tango)")',
-    'parenthetical_subgenre("(Sittin on the) Dock of the Bay")'
+    'parenthetical_subgenre("(Sittin on the) Dock of the Bay")',
+    'parenthetical_subgenre("Pandora (Hardstyle)")',
+    'parenthetical_subgenre("CERAMIC GIRL (Mathrock-Trap)")',
+    'parenthetical_subgenre("Replicant (instrumental electronic)")',
+    'parenthetical_subgenre("Chasing Suns (Alt Rock)")',
+    'parenthetical_subgenre("Trust In Computers (psychedelic rock)")',
+    'parenthetical_subgenre("Babel A.I. (Psytrance Metal Fusion)")',
+    'parenthetical_subgenre("Euphoria (Electronic/Djent/Experimental)")',
+    'parenthetical_subgenre("Cathedral of Pleasure (Electronic / Spokenword)")'
     ])
