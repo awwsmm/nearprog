7ad53715d9b5eb322b3652a2fe0461a7f1caba81
Andrew Watson <aww@awwsmm.com>
2021-02-20 21:54:49 +0000

    in monthly playlist, filter out songs which appeared in recent months' playlists

--- scripts/monthly_playlist.py
+++ scripts/monthly_playlist.py
@@ -1,4 +1,4 @@
-import os, sys, json, re, datetime, pathlib
+import os, sys, json, re, datetime, pathlib, glob
 
 from tools import reddit
 from tools import extract
@@ -20,12 +20,31 @@ basedir = pathlib.Path(__file__).parent
 #===============================================================================
 
 def playlist(test_mode = True):
-    dt = datetime.datetime.today()
 
-    if (test_mode):
-        outfile = sys.stdout
-    else:
-        outfile = (basedir / f'../monthly_playlist/{dt.year}-{dt.month:02}-{dt.day:02}.txt').open('w')
+    #---------------------------------------------------------------------------
+    #  check if song appears in the last three months of playlists
+    #---------------------------------------------------------------------------
+
+    def read_to_string(filename):
+        output = ""
+        with open(filename) as file:
+            output = file.read().replace('\n', '')
+        return output
+
+    # get the last three months (~90 days) of playlists, from most to least recent
+    recent_files = sorted(glob.glob(str(basedir / f'../monthly_playlist/*.txt')), reverse=True)[:3]
+    playlist_names = list(map(lambda x: x.rsplit('/', 1)[-1], recent_files))
+    recent_playlists = list(map(lambda x: read_to_string(x), recent_files))
+
+    def is_too_recent(title):
+        for index in range(len(playlist_names)):
+            if (title in recent_playlists[index]):
+                return (True, index)
+        return (False, -1)
+
+    #---------------------------------------------------------------------------
+    #  check if song appears in list of banned songs
+    #---------------------------------------------------------------------------
 
     with (basedir / '../json/banned_songs.json').open('r') as f:
         banned_songs = json.load(f)
@@ -36,6 +55,17 @@ def playlist(test_mode = True):
                 return True
         return False
 
+    #---------------------------------------------------------------------------
+    #  loop over top songs from the past month, filtering as we go
+    #---------------------------------------------------------------------------
+
+    dt = datetime.datetime.today()
+
+    if (test_mode):
+        outfile = sys.stdout
+    else:
+        outfile = (basedir / f'../monthly_playlist/{dt.year}-{dt.month:02}-{dt.day:02}.txt').open('w')
+
     counter = 1
     for submission in reddit.nearprog().top("month"):
         if (reddit.submission_is_song(submission)):
@@ -44,8 +74,12 @@ def playlist(test_mode = True):
             if (is_banned(artist, song)):
                 print(f' ---)  --^ | [[BANNED]] {submission.title}', file=outfile)
             else:
-                print(f' {counter:3})  {submission.score:2}^ | {submission.title}', file=outfile)
-                counter += 1
+                too_recent, playlist_index = is_too_recent(submission.title)
+                if (too_recent):
+                    print(f' ---)  --^ | [[RECENT]] ({playlist_names[playlist_index]}) {submission.title} ')
+                else:
+                    print(f' {counter:3})  {submission.score:2}^ | {submission.title}', file=outfile)
+                    counter += 1
 
     if (not test_mode):
         outfile.close()
